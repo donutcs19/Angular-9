8454856db336ec41aeff5d646f19760679e6a2e6
ShiKikie <toon1236@gmail.com>
2024-08-19 21:40:20 +0700

    add Component, 1 2 binding, ngModel

---
+++ Angular2024/app/src/app/employee/employee.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-employee',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './employee.component.html',
+  styleUrl: './employee.component.css'
+})
+export class EmployeeComponent {
+name = 'shikikie';
+cssClass = 'myClass';
+}
