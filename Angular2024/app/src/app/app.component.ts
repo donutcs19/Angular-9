9648d0a59b543b69b31af8d05a339569394ae779
ShiKikie <toon1236@gmail.com>
2024-08-19 21:42:24 +0700

    add Component, 1 2 binding, ngModel

--- Angular2024/app/src/app/app.component.ts
+++ Angular2024/app/src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, input, model } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeComponent } from './employee/employee.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EmployeeComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'app';
+  isAdmin = false;
+  brands: string[] = ['Mclaren','Tesla','Lamborghini','Ferrari'];
+
+  fname: string = '';
+  lname: string = '';
+
+  myFunction = () => {
+    alert('my Alert here');
+  }
+
+  checked =  model(false);
+  disabled = input(false);
+
+  toggle() {
+    this.checked.set(this.checked());
+  }
 }
